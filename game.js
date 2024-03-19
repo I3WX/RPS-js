69a95ca296ce3ff5c0fac593b7bbff2f7721bcb4
I3WX <utkarshs7828@gmail.com>
2024-03-19 16:27:43 +0530

    Fix bug in get_pc_choice

--- game.js
+++ game.js
@@ -1,41 +1,50 @@
-const choice = ['rock','paper','scissor'];
+
 
 function main(){
     let user_choice = get_user_choice();
     let pc_choise = get_pc_choice();
+    check_the_winner(user_choice,pc_choise)
 }
 
 function get_user_choice(){
-    let choice = prompt('Rock\nPaper\nScissor\n:');
-    return choice.toLowerCase();
+    let choice = prompt('Rock\nPaper\nScissor\n(q to exit):');
+    if (choice == 'q' || choice == 'Q'){
+        return choice.toLowerCase();
+    }
 }
 function get_pc_choice(){
-    choice = choice[Math.floor(Math.random()* choice.length)];
-    return choice.toLowerCase();
+    const choice = ['rock' ,'paper','scissor'];
+    index = Math.floor(Math.random()* choice.length);
+    console.log(choice[index])
+    return choice[index];
+
 }
 function check_the_winner(user_choice , pc_choise){
     if(user_choice == pc_choise){
-        console.log(`you and pc bot choose ${pc_choise}\nWhat a coincidence😅`)
+        alert(`you and pc bot choose ${pc_choise}\nWhat a coincidence😅`)
     }
     else if (user_choice == 'rock' && pc_choise == 'scissor'){
-        console.log("your rock beat pc's paper")
+        alert("your rock beat pc's paper")
     }
     else if (user_choice == 'paper' && pc_choise == 'rock'){
-        console.log("your paper beat pc's rock")
+        alert("your paper beat pc's rock")
     }
     else if (user_choice == 'scissor' && pc_choise == 'paper'){
-        console.log("your scissor beat pc's paper")
+        alert("your scissor beat pc's paper")
     }
     else if (user_choice == 'scissor' && pc_choise == 'rock'){
-        console.log("pc's rock beat yours scissor")
+        alert("pc's rock beat yours scissor")
     }
     else if (user_choice == 'rock' && pc_choise == 'paper'){
-        console.log("pc's paper beat yours rock")
+        alert("pc's paper beat yours rock")
     }
     else if (user_choice == 'paper' && pc_choise == 'scissor'){
-        console.log("pc's scissor beat yours paper")
+        alert("pc's scissor beat yours paper")
     }
     else{
-        console.log('System Error')
+        alert('System Error')
     }
 }
+
+
+main()
