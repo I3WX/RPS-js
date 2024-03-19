a44d7a769188689c7e702e7e2d1e2fc668f428f5
I3WX <utkarshs7828@gmail.com>
2024-03-19 16:15:25 +0530

    Created check winner function to decide winner of game

--- game.js
+++ game.js
@@ -13,3 +13,29 @@ function get_pc_choice(){
     choice = choice[Math.floor(Math.random()* choice.length)];
     return choice.toLowerCase();
 }
+function check_the_winner(user_choice , pc_choise){
+    if(user_choice == pc_choise){
+        console.log(`you and pc bot choose ${pc_choise}\nWhat a coincidence😅`)
+    }
+    else if (user_choice == 'rock' && pc_choise == 'scissor'){
+        console.log("your rock beat pc's paper")
+    }
+    else if (user_choice == 'paper' && pc_choise == 'rock'){
+        console.log("your paper beat pc's rock")
+    }
+    else if (user_choice == 'scissor' && pc_choise == 'paper'){
+        console.log("your scissor beat pc's paper")
+    }
+    else if (user_choice == 'scissor' && pc_choise == 'rock'){
+        console.log("pc's rock beat yours scissor")
+    }
+    else if (user_choice == 'rock' && pc_choise == 'paper'){
+        console.log("pc's paper beat yours rock")
+    }
+    else if (user_choice == 'paper' && pc_choise == 'scissor'){
+        console.log("pc's scissor beat yours paper")
+    }
+    else{
+        console.log('System Error')
+    }
+}
